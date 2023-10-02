ca87dfffe228bf0bc8d94b76fe04e618dc253c7d
DavideDev1998 <146113038+DavideDev1998@users.noreply.github.com>
2023-10-02 12:19:41 +0200

    Add files via upload

---
+++ Meme_Book/src/main/java/com/project/Meme_Book/MemeBookApplication.java
@@ -0,0 +1,13 @@
+package com.project.Meme_Book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeBookApplication.class, args);
+	}
+
+}
---
+++ Meme_Book/src/test/java/com/project/Meme_Book/MemeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Meme_Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
