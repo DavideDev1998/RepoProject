c430c71e691aeca121ac4187e36528ab4c6e3c39
Coco <francescazannella91@gmail.com>
2023-10-04 10:26:28 +0200

    Add Repository annotation

--- Meme_Book/src/main/java/com/project/Meme_Book/repository/LikeRepository.java
+++ Meme_Book/src/main/java/com/project/Meme_Book/repository/LikeRepository.java
@@ -2,6 +2,8 @@ package com.project.Meme_Book.repository;
 
 import com.project.Meme_Book.model.Like;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepository  extends MongoRepository<Like, String> {
 }
--- Meme_Book/src/main/java/com/project/Meme_Book/repository/PreferitoRepository.java
+++ Meme_Book/src/main/java/com/project/Meme_Book/repository/PreferitoRepository.java
@@ -2,6 +2,8 @@ package com.project.Meme_Book.repository;
 
 import com.project.Meme_Book.model.Preferito;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PreferitoRepository extends MongoRepository<Preferito, String> {
 }
