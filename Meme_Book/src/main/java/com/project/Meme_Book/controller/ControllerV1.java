f99da7afa23c7ed747d3ed4b873240525660d3db
Davide tafuri <Davidello98@hotmail.it>
2023-10-05 09:37:02 +0200

    Add Class StringUtils and Method insertContent in ControllerV1

--- Meme_Book/src/main/java/com/project/Meme_Book/controller/ControllerV1.java
+++ Meme_Book/src/main/java/com/project/Meme_Book/controller/ControllerV1.java
@@ -3,16 +3,21 @@ package com.project.Meme_Book.controller;
 import com.project.Meme_Book.model.Argomento;
 import com.project.Meme_Book.model.Content;
 import com.project.Meme_Book.model.User;
+import com.project.Meme_Book.model.dto.RequestData;
 import com.project.Meme_Book.service.impl.*;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+@Log
 @RestController
+@RequestMapping("/CreationContent")
 public class ControllerV1 {
 
     @Autowired
@@ -39,47 +44,10 @@ public class ControllerV1 {
     @GetMapping("/f")
     public List<Content> findAllDocument() {return contentRepository.findAll();}
 
-    @GetMapping("/")
-    public void insertDocument() {
+    @GetMapping("/insert")
+    public void insertContent(@RequestBody RequestData request ) {
 
-        Date date = new Date();
 
-        User user1 = new User("1", "Francesca", "Zannella", 25, "Female", "Coco", "francescazan@live.com", "CocoBandicoot97!", "Admin");
-        User user2 = new User("2", "Davide", "Tafuri", 26, "Male", "Puzzone", "davidello98@hotmail.it", "CocoBandicoot97!", "Admin");
-        User user3 = new User("3", "Tizio", "Caio", 25, "Male", "TizioCaio", "tizioecaio@gmail.com", "OkIlPrezzoeGiusto002!", "User");
-
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
-
-        Argomento argomento1 = new Argomento("1", "Argomento_1",null);
-        Argomento argomento2 = new Argomento("2", "Argomento_2", null);
-        Argomento argomento3 = new Argomento("3", "Argomento_3", null);
-
-        argsRepository.save(argomento1);
-        argsRepository.save(argomento2);
-        argsRepository.save(argomento3);
-
-        Content content1 = new Content("1", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.milanotoday.it%2Fattualita%2Ftramonto-infuocato-rosso-3-settembre-2023.html&psig=AOvVaw0icNVmD6RqGPlOkT_RLilM&ust=1696508841702000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDsx4iy3IEDFQAAAAAdAAAAABAE",
-                                       "Tramonto",date,date,null,null,50, user1,argomento1);
-        Content content2 = new Content("2", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.milanotoday.it%2Fattualita%2Ftramonto-infuocato-rosso-3-settembre-2023.html&psig=AOvVaw0icNVmD6RqGPlOkT_RLilM&ust=1696508841702000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDsx4iy3IEDFQAAAAAdAAAAABAE",
-                "Tramonto",date,date,null,null,50, user2,argomento2);
-        Content content3 = new Content("3", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.milanotoday.it%2Fattualita%2Ftramonto-infuocato-rosso-3-settembre-2023.html&psig=AOvVaw0icNVmD6RqGPlOkT_RLilM&ust=1696508841702000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDsx4iy3IEDFQAAAAAdAAAAABAE",
-                "Tramonto",date,date,null,null,50, user3,argomento3);
-
-        contentRepository.save(content1);
-        contentRepository.save(content2);
-        contentRepository.save(content3);
-
-        List<Content> contenutoList1 = new ArrayList<>();
-        contenutoList1.add(content1);
-                contenutoList1.add(content2);
-
-                List<Content> contenutoList2 = new ArrayList<>();
-        contenutoList2.add(content2);
-        contenutoList2.add(content3);
-
-        System.out.println("Documents inserito con successo.");
         }
         }
 
---
+++ Meme_Book/src/main/java/com/project/Meme_Book/model/dto/RequestData.java
@@ -0,0 +1,64 @@
+package com.project.Meme_Book.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.mongodb.lang.NonNull;
+import com.project.Meme_Book.model.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestData {
+    private String id_Argomento;
+    private String nameArgomento;
+    private List<Content> contenutoList_Argomento;
+    private String id_Commento;
+    private List<Like> like_Commento;0
+    private String commento;
+    private String id_Content;
+    private String url_Content;
+    private String didascalia_Content;
+    private Date creationDate_Content;
+    private Date modifyDate_Content;
+    private List<Commento> commentoList_Content;
+    private List<Like> like_Content;
+    private Integer condivisioni_Content;
+    private User creator;
+    private Argomento argomento;
+    private String id_Like;
+    private User user;
+    private String id_Preferito;
+    private User userName;
+    private ArrayList<Content> contents_Preferito;
+    private String id_Profilizione;
+    private User userName_Profilazione;
+    private List<Like> like__Profilazione;
+    private Argomento nomeArgomento_Profilazione;
+    private String id_Segnalazione;
+    private Content content;
+    private User user_Segnalazione;
+    private User creator_Segnalazione;
+    private String url_Segnalazione;
+    private String id_User;
+    private String firstName_User;
+    private String lastName_User;
+    private Integer age_User;
+    private String sex_User;
+    private String userName_User;
+    private String email_User;
+    private String password_User;
+    private String roleName_User;
+    private String id_ValidationCheck;
+    private String userName_ValidationCheck;
+    private Boolean check_ValidationCheck;
+
+
+}
